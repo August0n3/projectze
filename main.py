1133ab0b8fd7968b5f76136d43cec2c843664714
Augusto <august0n3211@gmail.com>
2021-05-07 03:36:11 -0300

    inicio real do desenvolvimento do bot.

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import discord 
+import asyncio
+from models.streamer import Streamer
 
 client = discord.Client()
 
+cor= '#39CACF'
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Bot online - Olá, Mundo!')
+    print(client.user)
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!dark'):
-        await message.channel.send('O maior lixo do server')
-
-    if message.content.startswith('!nishida'):
-        await message.channel.send('gatinho rawr miau >///<')
+    if message.content.lower().startswith('<help'):
+        embed = discord.Embed(
+            title= 'Bem vindo ao José bot! O bot que simula a vida de um streamer!',
+            description="Para registrar-se digite - <register \n"
+                        "Para ir para a loja digite - <store\n"
+                        "Para ver seus status digite - <status\n"
+                        "Para começar uma stream digite- <stream\n"
+        )
+    botmsg= await message.channel.send(embed=embed)
 
 
 client.run('ODQwMDU1MDk1OTcwMjM0Mzk5.YJSoRg.Pr6dv_758lzs9s-INR6oeOAFGV0')
---
+++ models/streamer.py
@@ -0,0 +1,7 @@
+class Streamer():
+    def __init__(self,id,level,stamina,subs,money):
+        self.id = id
+        self.level = 0
+        self.stamina = 50
+        self.subs = 0
+        self.money = 1000
