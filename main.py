2bc48ab1effc24c9d17187a8e76998d9daf8e017
Augusto <august0n3211@gmail.com>
2021-05-07 00:54:52 -0300

    adição do banco de dados(ainda vazio, e modificações no main(apenas testes ainda nada funcional)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -15,6 +16,12 @@ async def on_message(message):
         await message.channel.send('O maior lixo do server')
 
     if message.content.startswith('!nishida'):
-        await message.channel.send('fofinho :3')
+        await message.channel.send('gatinho rawr miau >///<')
+
+    if message.content.startswith('!avatar'):
+        avatar = discord.User(avatar_url)
+        await message.channel.send(avatar)
+
+    
 
 client.run('ODQwMDU1MDk1OTcwMjM0Mzk5.YJSoRg.Pr6dv_758lzs9s-INR6oeOAFGV0')
